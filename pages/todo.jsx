b29f78d226470014885a75cd292a84cb208abdf3
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2025-07-31 19:32:29 +0400

    первоначально работает

--- pages/todo.jsx
+++ pages/todo.jsx
@@ -1,5 +1,8 @@
-export default function ToDoPage(){
+import { ToDoApp } from '@/components/todo';
+
+export default function ToDoPage() {
   return <>
-  <h1>To Do List</h1>
+    <h1>To Do List</h1>
+    <ToDoApp />
   </>
 }
